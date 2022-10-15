d6b4a5f0c3ba07bb4f0832e72ec6f6c22fd78e6e
andrebortolon <andrebortolon@hotmail.com>
2022-10-15 00:07:08 -0300

    Teste de conexão do Eclipse com Git

---
+++ TesteProjetoGit/src/com/dio/TesteProjetoGit.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+public class TesteProjetoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Apenas um teste para usar o git no Eclipse");
+
+	}
+
+}
