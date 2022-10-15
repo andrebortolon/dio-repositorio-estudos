caaa802a29916e3dc8a9d49fb149529ffb8b2985
andrebortolon <andrebortolon@hotmail.com>
2022-10-15 00:14:51 -0300

    Novo teste

--- TesteProjetoGit/src/com/dio/TesteProjetoGit.java
+++ TesteProjetoGit/src/com/dio/TesteProjetoGit.java
@@ -3,7 +3,6 @@ package com.dio;
 public class TesteProjetoGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Apenas um teste para usar o git no Eclipse");
 
 	}
